d53a14a50b402e22806887fbc2deb57cd69eb8bd
Akrep356 <halit.yilmazz64@gmail.com>
2026-09-18 23:00:21 +0300

    Change displayed text in MainActivity
    Updated text displayed in MainActivity to 'NOVA'.

--- app/src/main/Java/com/example/nova/MainActivity.kt
+++ app/src/main/Java/com/example/nova/MainActivity.kt
@@ -1,10 +1,8 @@
 package com.example.nova
 
 import android.os.Bundle
-import
-androidx.activity.ComponentActivity
-import
-androidx.activity.compose.setContent
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.compose.materia13.Text
 
 class MainActivity :
@@ -17,8 +15,8 @@ onCreate(savedInstanceState: Bundle?)
 super.onCreate(savedInstanceState)
 
         setContent {
-            Text("NOVA: AI Müzik,
-Video ve Sanatçı")
-                 }
-                 }
-                 }
+            Text(text = "NOVA")
+        }
+
+  }
+}
