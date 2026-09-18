3d0847e46a9a4833f84db3cb054b33abc5f590fb
Akrep356 <halit.yilmazz64@gmail.com>
2026-09-18 22:47:10 +0300

    Add MainActivity with basic UI setup

---
+++ app/src/main/Java/com/example/nova/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.nova
+
+import android.os.Bundle
+import
+androidx.activity.ComponentActivity
+import
+androidx.activity.compose.setContent
+import androidx.compose.materia13.Text
+
+class MainActivity :
+ComponentActivity() {
+    override fun
+onCreate(savedInstanceState: Bundle?)
+  {
+
+super.onCreate(savedInstanceState)
+
+        setContent {
+            Text("NOVA: AI Müzik,
+Video ve Sanatçı")
+                 }
+                 }
+                 }
